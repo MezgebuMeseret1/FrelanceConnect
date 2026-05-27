f2bf4e2adb7e463cd90fcbc81c4e5a236964621f
Mezgebuu Meseret <mezgebumeseret806@gmail.com>
2026-05-27 23:35:27 +0300

    Add missing milestone service exports

--- frelanceconnect-frontend/src/core/services/milestone.service.js
+++ frelanceconnect-frontend/src/core/services/milestone.service.js
@@ -21,3 +21,27 @@ export const createMilestone = async (data) => {
 
   return response.data;
 };
+export const updateMilestone = async (id, data) => {
+  const response = await apiClient.put(`${BASE_URL}/${id}`, data);
+  return response.data;
+};
+
+export const updateMilestoneStatus = async (id, status) => {
+  const response = await apiClient.patch(`${BASE_URL}/${id}/status`, { status });
+  return response.data;
+};
+
+export const submitMilestone = async (id, submissionData) => {
+  const response = await apiClient.post(`${BASE_URL}/${id}/submit`, submissionData);
+  return response.data;
+};
+
+export const approveMilestone = async (id) => {
+  const response = await apiClient.post(`${BASE_URL}/${id}/approve`);
+  return response.data;
+};
+
+export const releaseMilestonePayment = async (id) => {
+  const response = await apiClient.post(`${BASE_URL}/${id}/release-payment`);
+  return response.data;
+};
