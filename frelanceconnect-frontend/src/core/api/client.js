9dca332e466da4bcd0c5f3fe99e9f7268db75e45
Mezgebuu Meseret <mezgebumeseret806@gmail.com>
2026-05-28 08:38:20 +0300

    fix  login3

--- frelanceconnect-frontend/src/core/api/client.js
+++ frelanceconnect-frontend/src/core/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://frelanceconnect.onrender.com/api',
+   baseURL: 'https://frelanceconnect.onrender.com/api/v1',
   
 });
 
